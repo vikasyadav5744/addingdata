4cd6d51034941c451221ce259352d87e721e48ba
vikasyadav5744 <vikram.kumar500@gmail.com>
2025-09-14 08:51:47 +0530

    Update Add.py

--- Add.py
+++ Add.py
@@ -1,11 +1,11 @@
-import pandas
+import pandas as pd
 import streamlit as st
 import numpy as np
 st.write("addind data to github excel page")
 file_name='https://github.com/vikasyadav5744/vikasyadav5744/blob/main/sample.xlsx'
 upload=st.file_uploader("csv file upload here")
 if upload !=None:
-  #master = pd.read_excel(file_name)
+  master = pd.read_excel(file_name)
   newfile=pd.read_csv(upload, skiprows=1)
   st.write(newfile)
   st.write(master)
